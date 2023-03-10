ab611bb10f8e26e90e832d9ff032ac23dc19da61
Gonza Medrano <gonzalomedrano09@gmail.com>
2023-03-09 21:03:54 -0300

    Add main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gonzamedrano09/chat/pkg/config"
+	"github.com/gonzamedrano09/chat/pkg/infrastructure"
+	"github.com/gonzamedrano09/chat/pkg/registry"
+)
+
+func main() {
+	config.LoadConfig()
+	database := infrastructure.NewDatabase()
+
+	r := registry.NewRegistry(database)
+	app := r.NewAppController()
+
+	router := gin.Default()
+	infrastructure.NewRouter(router, &app)
+	router.Run("localhost:8080")
+}
